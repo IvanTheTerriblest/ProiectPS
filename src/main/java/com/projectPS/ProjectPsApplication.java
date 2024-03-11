b2985c8f0437183e3bebc731efd27f488522f97b
Ivan Alexandru <alexandru_ivan91@yahoo.com>
2024-03-11 11:36:14 +0200

    Documentatie

---
+++ src/main/java/com/projectPS/ProjectPsApplication.java
@@ -0,0 +1,13 @@
+package com.projectPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectPS/ProjectPsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
