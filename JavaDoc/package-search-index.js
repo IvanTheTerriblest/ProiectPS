3188714fabf17287ddf755155ed99813a0306a31
Ivan Alexandru <alexandru_ivan91@yahoo.com>
2024-03-31 23:44:59 +0300

    update gitignore x2

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.projectPS"},{"l":"com.projectPS.Config"},{"l":"com.projectPS.Controller"},{"l":"com.projectPS.Model"},{"l":"com.projectPS.Repository"},{"l":"com.projectPS.Service"},{"l":"com.projectPS.Util"}];updateSearchResults();
